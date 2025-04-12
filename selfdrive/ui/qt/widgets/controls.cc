91e3c9eda494d6323210d57a3123b1903ffb36f0
Sunwolf <91574917+Sage-A@users.noreply.github.com>
2025-04-12 12:18:03 -0500

    Update controls.cc

--- selfdrive/ui/qt/widgets/controls.cc
+++ selfdrive/ui/qt/widgets/controls.cc
@@ -139,3 +139,26 @@ void ParamControl::toggleClicked(bool state) {
     toggle.togglePosition();
   }
 }
+
+SliderControl::SliderControl(const QString &title, const QString &text, const QString &desc, QWidget *parent) : AbstractControl(title, desc, "", parent) {
+  btn.setText(text);
+  btn.setStyleSheet(R"(
+    QPushButton {
+      padding: 0;
+      border-radius: 50px;
+      font-size: 35px;
+      font-weight: 500;
+      color: #E4E4E4;
+      background-color: #393939;
+    }
+    QPushButton:pressed {
+      background-color: #4a4a4a;
+    }
+    QPushButton:disabled {
+      color: #33E4E4E4;
+    }
+  )");
+  btn.setFixedSize(250, 100);
+  QObject::connect(&btn, &QPushButton::clicked, this, &SliderControl::clicked);
+  hlayout->addWidget(&btn);
+}
