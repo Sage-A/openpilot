cb9d1ac7abb6f10132eb6e9259eb3de1434ebc4f
Sunwolf <91574917+Sage-A@users.noreply.github.com>
2025-04-19 17:53:54 -0500

    Update customGUI.cc

--- selfdrive/ui/qt/offroad/customGUI.cc
+++ selfdrive/ui/qt/offroad/customGUI.cc
@@ -18,7 +18,7 @@
 
 speedSlider::speedSlider(QWidget* parent) : QWidget(parent) {
   QHBoxLayout *main = new QHBoxLayout(this);
-  QLabel title = new QLabel(Qtext("Speed Control");
+  QLabel title = new QLabel(QString("Speed Control");
 
   main->addWidget(title);
   SliderControl speed_slider = new SliderControl(Qt::Horizontal);
