49fc8e4ee37232b4b966225fccfe9d680de829d5
Sunwolf <91574917+Sage-A@users.noreply.github.com>
2025-04-22 02:26:38 -0500

    Update customGUI.cc

--- selfdrive/ui/qt/offroad/customGUI.cc
+++ selfdrive/ui/qt/offroad/customGUI.cc
@@ -229,7 +229,7 @@ StatusWidget::StatusWidget(QWidget* parent) : QWidget(parent) {
 void StatusWidget::update(const SubMaster &sm) {
   fuel_value->setText(QString::number(sm["carState"].getCarState().getFuelGauge()));
   door_value->setText(QString::number(sm["carState"].getCarState().getDoorOpen()));
-  if(QString::number(sm["carState"].getCarState().getGearShifter()) == 0){
+  if(sm["carState"].getCarState().getGearShifter().get()){
     gear_value->setText("Drive");
   }
   elseif(sm["carState"].getCarState().getGearShifter() == ::cereal::CarState::GearShifter().park){
