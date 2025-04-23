58003628dc36a69260566dca20333cf147cb2e98
Sunwolf <91574917+Sage-A@users.noreply.github.com>
2025-04-22 23:42:24 -0500

    Update customGUI.cc

--- selfdrive/ui/qt/offroad/customGUI.cc
+++ selfdrive/ui/qt/offroad/customGUI.cc
@@ -247,7 +247,7 @@ StatusWidget::StatusWidget(QWidget* parent) : QWidget(parent) {
 }
 
 void StatusWidget::update(const SubMaster &sm) {
-  CarState cs = sm["carState"].getCarState();
+  ceral::CarState cs = sm["carState"].getCarState();
   fuel_value->setText(QString::number(cs.getGas()));
   gas_value->setText(QString::number(cs.getGasPressed()));
   door_value->setText(QString::number(cs.getDoorOpen()));
