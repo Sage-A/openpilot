11e6961a70f211bb8b225f826fa280cba06ebdfb
Sunwolf <91574917+Sage-A@users.noreply.github.com>
2025-04-24 00:22:18 -0500

    Update acceleration.cc

--- selfdrive/ui/qt/offroad/custom_widgets/acceleration.cc
+++ selfdrive/ui/qt/offroad/custom_widgets/acceleration.cc
@@ -1,6 +1,21 @@
+#include <cassert>
+#include <cmath>
 #include <string>
+#include <tuple>
+#include <vector>
+
 #include <QDebug>
 
+#include "common/watchdog.h"
+#include "common/util.h"
+#include "selfdrive/ui/qt/network/networking.h"
+#include "selfdrive/ui/qt/offroad/settings.h"
+#include "selfdrive/ui/qt/qt_window.h"
+#include "selfdrive/ui/qt/widgets/prime.h"
+#include "selfdrive/ui/qt/widgets/scrollview.h"
+#include "selfdrive/ui/qt/offroad/developer_panel.h"
+#include "selfdrive/ui/qt/offroad/customGUI.h"
+#include "cereal/gen/cpp/car.capnp.h"
 #include "selfdrive/ui/qt/offroad/custom_widgets/acceleration.h"
 
 AccelerationW::AccelerationW(QWidget* parent) : QWidget(parent) {
