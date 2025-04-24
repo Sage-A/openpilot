a7fcfc1639f065f9a7a9ade323dfdf488c1133c2
Sunwolf <91574917+Sage-A@users.noreply.github.com>
2025-04-23 23:59:30 -0500

    Update acceleration.cc

--- selfdrive/ui/qt/offroad/custom_widgets/acceleration.cc
+++ selfdrive/ui/qt/offroad/custom_widgets/acceleration.cc
@@ -1,1 +1,38 @@
+#include <string>
+#include "selfdrive/ui/qt/offroad/custom_widgets/acceleration.h"
 
+acceleration::acceleration(QWidget* parent) : QWidget(parent) {
+  QVBoxLayout *main = new QVBoxLayout(this);
+  QLabel *title2 = new QLabel("Acceleration");
+  title2->setStyleSheet(R"(
+    QLabel {
+     font-size: 40px;
+     font-weight: bold;
+     color: #a9a9a9;
+     }
+    )");
+  
+  QPushButton *incBtn = new QPushButton(tr("^"));
+  incBtn->setFixedSize(200, 350);
+  
+  QPushButton *decBtn = new QPushButton(tr("V"));
+  decBtn->setFixedSize(200, 350);
+  
+  main->addWidget(title2);
+  main->addWidget(incBtn);
+  main->addWidget(decBtn);
+
+   setStyleSheet(R"(
+    QPushButton {
+      font-size: 140px;
+      padding-bottom: 20px;
+      background-color: #DDDDDD;
+      font-weight: 400;
+    }
+    QPushButton:pressed {
+      background-color: #3B3B3B;
+    }
+  )");
+
+  main->setAlignment(Qt::AlignCenter);
+}
