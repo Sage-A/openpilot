02c96d370151c3fd86effa12fac33e00c9017ad5
Sunwolf <91574917+Sage-A@users.noreply.github.com>
2025-04-23 23:57:01 -0500

    Update acceleration.h

--- selfdrive/ui/qt/offroad/custom_widgets/acceleration.h
+++ selfdrive/ui/qt/offroad/custom_widgets/acceleration.h
@@ -1,1 +1,19 @@
+#pragma once
 
+#include <map>
+#include <string>
+
+#include <QFrame>
+#include <QLabel>
+#include <QPushButton>
+#include <QWidget>
+#include <QPixmap>
+
+#include "selfdrive/ui/ui.h"
+#include "selfdrive/ui/qt/util.h"
+
+class accelWidget : public QWidget {
+  Q_OBJECT
+public:
+  explicit accelWidget(QWidget *parent);
+};
