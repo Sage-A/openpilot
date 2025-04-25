eb93d9d63f1eaae270d80986f6b420bc8b8614d4
Sunwolf <91574917+Sage-A@users.noreply.github.com>
2025-04-25 14:39:58 -0500

    Update customGUI.h

--- selfdrive/ui/qt/offroad/customGUI.h
+++ selfdrive/ui/qt/offroad/customGUI.h
@@ -17,27 +17,9 @@
 #include "selfdrive/ui/qt/widgets/controls.h"
 #include "selfdrive/ui/qt/offroad/custom_widgets/acceleration.h"
 #include "selfdrive/ui/qt/offroad/custom_widgets/steering.h"
+#include "selfdrive/ui/qt/offroad/custom_widgets/status.h"
 
 // ********** custom window + top-level panels **********
-class StatusWidget : public QWidget {
-  Q_OBJECT
-public:
-  explicit StatusWidget(QWidget *parent);
-  void update(const SubMaster &sm);
-
-private:
-  QLabel *fuel_value;
-  QLabel *door_value;
-  QLabel *seatbelt_value;
-  QLabel *gas_value;
-  QLabel *brake_value;
-  QLabel *ss_value;
-  QLabel *steer_enabled;
-  QLabel *steer_value;
-  QLabel *esp_value;
-  QLabel *cruise_value;
-  QLabel *gear_value;
-};
 
 class speedWidget : public QWidget {
   Q_OBJECT
