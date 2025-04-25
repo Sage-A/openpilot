1e6f5e5308a8b40ec73b0e7641dc8cb9fad8775f
Sunwolf <91574917+Sage-A@users.noreply.github.com>
2025-04-25 14:18:42 -0500

    Update steering.h

--- selfdrive/ui/qt/offroad/custom_widgets/steering.h
+++ selfdrive/ui/qt/offroad/custom_widgets/steering.h
@@ -1,1 +1,27 @@
+#pragma once
 
+#include <map>
+#include <string>
+
+#include <QButtonGroup>
+#include <QFrame>
+#include <QLabel>
+#include <QPushButton>
+#include <QStackedWidget>
+#include <QWidget>
+#include <QSpinBox>
+#include <QPixmap>
+
+#include "selfdrive/ui/ui.h"
+#include "selfdrive/ui/qt/util.h"
+#include "selfdrive/ui/qt/widgets/controls.h"
+
+class steeringSlider : public QWidget {
+  Q_OBJECT
+public:
+  explicit steeringSlider(QWidget *parent);
+
+private:
+  SliderControl *steering_slider;
+  QSpinBox *turnAngle;
+};
