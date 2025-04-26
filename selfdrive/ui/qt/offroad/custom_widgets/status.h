30945523b7940bda0bf444ef031878aec1e662c4
Sunwolf <91574917+Sage-A@users.noreply.github.com>
2025-04-26 15:40:07 -0500

    Update status.h

--- selfdrive/ui/qt/offroad/custom_widgets/status.h
+++ selfdrive/ui/qt/offroad/custom_widgets/status.h
@@ -23,9 +23,8 @@ class StatusWidget : public QWidget {
    void update(const SubMaster &sm);
  
  private:
+   QVBoxLayout *main;
    QLabel *fuel_value;
-   QLabel *door_value;
-   QLabel *seatbelt_value;
    QLabel *gas_value;
    QLabel *brake_value;
    QLabel *ss_value;
@@ -35,3 +34,60 @@ class StatusWidget : public QWidget {
    QLabel *cruise_value;
    QLabel *gear_value;
  };
+
+class SpeedStatus : public QWidget {
+   Q_OBJECT
+ public:
+   explicit SpeedStatus(QWidget *parent);
+   void update(const SubMaster &sm);
+ 
+ private: // Current speed of car, standstill indicator
+   QVBoxLayout *main;
+   QLabel *indicator;
+   QLabel *unit;
+};
+
+class BlinkerStatus : public QWidget {
+   Q_OBJECT
+ public:
+   explicit BlinkerStatus(QWidget *parent);
+   void update(const SubMaster &sm);
+ 
+ private: // left/right blinkers on and off
+   QVBoxLayout *main;
+   QPixmap iconMap[2];
+   QLabel *leftInd;
+   QLabel *rightInd;
+};
+
+class CarStatus : public QWidget {
+   Q_OBJECT
+ public:
+   explicit CarStatus(QWidget *parent);
+   void update(const SubMaster &sm);
+ 
+ private: // doors open, seatbelt unlatched, esp enabled
+   QVBoxLayout *main;
+   QLabel *door_value;
+   QLabel *seatbelt_value;
+};
+
+class DriveStatus : public QWidget {
+   Q_OBJECT
+ public:
+   explicit DriveStatus(QWidget *parent);
+   void update(const SubMaster &sm);
+ 
+ private: // Gas pedal, gas pressed, brake pressed, current gear, cruise control enabled
+   QVBoxLayout *main;
+};
+
+class SteerStatus : public QWidget {
+   Q_OBJECT
+ public:
+   explicit SteerStatus(QWidget *parent);
+   void update(const SubMaster &sm);
+ 
+ private: // Steering value, steering engaged
+   QVBoxLayout *main;
+};
