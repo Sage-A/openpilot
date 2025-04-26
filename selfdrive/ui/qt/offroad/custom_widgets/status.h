44fde0c60a8a7dc7aafe4980732d7cd87f89637a
Sunwolf <91574917+Sage-A@users.noreply.github.com>
2025-04-26 14:55:05 -0500

    Update status.h

--- selfdrive/ui/qt/offroad/custom_widgets/status.h
+++ selfdrive/ui/qt/offroad/custom_widgets/status.h
@@ -1,3 +1,20 @@
+#pragma once
+
+#include <map>
+#include <string>
+
+#include <QButtonGroup>
+#include <QFrame>
+#include <QLabel>
+#include <QPushButton>
+#include <QStackedWidget>
+#include <QWidget>
+#include <QSpinBox>
+#include <QPixmap>
+
+#include "selfdrive/ui/ui.h"
+#include "selfdrive/ui/qt/util.h"
+#include "selfdrive/ui/qt/widgets/controls.h"
 
 class StatusWidget : public QWidget {
    Q_OBJECT
