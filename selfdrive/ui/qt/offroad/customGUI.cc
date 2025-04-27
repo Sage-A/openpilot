fc1f1115a2ccf9fa8606561a7aa99fe9ad3a9a92
Sunwolf <91574917+Sage-A@users.noreply.github.com>
2025-04-27 14:13:35 -0500

    Update customGUI.cc

--- selfdrive/ui/qt/offroad/customGUI.cc
+++ selfdrive/ui/qt/offroad/customGUI.cc
@@ -1,3 +1,4 @@
+#include <Python.h>
 #include <cassert>
 #include <cmath>
 #include <string>
@@ -147,7 +148,10 @@ CustomWindow::CustomWindow(QWidget *parent) : QFrame(parent) {
   });
   QObject::connect(close_btn, &QPushButton::clicked, this, &CustomWindow::closeCustom);
   try {
-    connect(accW, &AccelerationW::accelStart, [=]() { emit accelDown(); });
+    connect(accW, &AccelerationW::accelStart, [=]() { 
+      Py_Initalize();
+      Py_Finalize();
+    });
     connect(accW, &AccelerationW::accelStop, [=]() { emit accelRelease(); });
     connect(ss, &SteeringSlider::valueChange, [=](int val) { emit emitSliderVal(val); });
   }
