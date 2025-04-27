943a1266e4d0aa58e532bb7c5c67e253fcf81463
Sunwolf <91574917+Sage-A@users.noreply.github.com>
2025-04-27 02:34:46 -0500

    Update steering.cc

--- selfdrive/ui/qt/offroad/custom_widgets/steering.cc
+++ selfdrive/ui/qt/offroad/custom_widgets/steering.cc
@@ -46,6 +46,7 @@ SteeringSlider::SteeringSlider(QWidget* parent) : QWidget(parent) {
   // Set style of slider
   steering_slider = new SliderControl(Qt::Horizontal);
   connect(steering_slider, &SliderControl::valueChanged, turnAngle, &QSpinBox::setValue);
+  QObject::connect(steering_slider, &SliderControl::valueChanged, this, &SteeringSlider::valueChanged);
   steering_slider->setStyleSheet(R"(
           QSlider {
           min-height: 200px;
