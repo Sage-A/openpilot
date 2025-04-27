72360cfdfff964ab3ceaf96585acc29438b5fd93
Sunwolf <91574917+Sage-A@users.noreply.github.com>
2025-04-27 01:57:09 -0500

    Update customGUI.cc

--- selfdrive/ui/qt/offroad/customGUI.cc
+++ selfdrive/ui/qt/offroad/customGUI.cc
@@ -17,6 +17,7 @@
 #include "selfdrive/ui/qt/offroad/customGUI.h"
 #include "cereal/gen/cpp/car.capnp.h"
 
+// Bring custom window to the front
 void CustomWindow::showEvent(QShowEvent *event) {
   setCurrentPanel(0);
 }
@@ -24,9 +25,10 @@ void CustomWindow::showEvent(QShowEvent *event) {
 void CustomWindow::setCurrentPanel(int index, const QString &param) {}
 
 CustomWindow::CustomWindow(QWidget *parent) : QFrame(parent) {
-
+  // Connect custom GUI update to parent uiState update function
   QObject::connect(uiState(), &UIState::uiUpdate, this, &CustomWindow::updateState);
-  
+
+  // Close btn returns to settings page
   QPushButton *close_btn = new QPushButton(tr("×"));
   close_btn->setStyleSheet(R"(
     QPushButton {
@@ -42,6 +44,7 @@ CustomWindow::CustomWindow(QWidget *parent) : QFrame(parent) {
   )");
   close_btn->setFixedSize(150, 150);
 
+  // Status control buttons to hide/show indicator groups
   QPushButton *blink_btn = new QPushButton(tr("Blinker"));
   blink_btn->setStyleSheet(R"(
     QPushButton {
@@ -129,6 +132,7 @@ CustomWindow::CustomWindow(QWidget *parent) : QFrame(parent) {
   speed_bar->addWidget(spW);
   speed_bar->addWidget(accW);
 
+  // Connect buttons to visibility functions + settings close
   QObject::connect(blink_btn, &QPushButton::clicked, [&](){
     lW->setVisible(!lW->isVisible());
   });
@@ -152,6 +156,8 @@ CustomWindow::CustomWindow(QWidget *parent) : QFrame(parent) {
   )");
 }
 
+// Update contents of custom window
+// Inherits from ui
 void CustomWindow::updateState(const UIState &s) {
   const SubMaster &sm = *(s.sm);
   lW->update(sm);
