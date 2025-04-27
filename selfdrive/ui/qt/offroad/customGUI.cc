336ede689819fb612935d16c9781a60f540a2660
Sunwolf <91574917+Sage-A@users.noreply.github.com>
2025-04-26 20:59:06 -0500

    Update customGUI.cc

--- selfdrive/ui/qt/offroad/customGUI.cc
+++ selfdrive/ui/qt/offroad/customGUI.cc
@@ -59,9 +59,9 @@ CustomWindow::CustomWindow(QWidget *parent) : QFrame(parent) {
   driveStat = new DriveStatus(this);
   
   primary->addWidget(lW);
-  primary->addWidget(carStat);
-  primary->addWidget(steerStat);
-  primary->addWidget(driveStat);
+  //primary->addWidget(carStat);
+  //primary->addWidget(steerStat);
+ // primary->addWidget(driveStat);
   primary->addWidget(ss);
 
   accW = new AccelerationW(this);
