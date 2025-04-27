86ed7fcc1983cf9bec6408b6d26fa6d2ca52306e
Sunwolf <91574917+Sage-A@users.noreply.github.com>
2025-04-27 14:10:05 -0500

    Update joystick_control.py

--- tools/joystick/joystick_control.py
+++ tools/joystick/joystick_control.py
@@ -10,7 +10,6 @@ from openpilot.common.params import Params
 from openpilot.common.realtime import Ratekeeper
 from openpilot.system.hardware import HARDWARE
 from openpilot.tools.lib.kbhit import KBHit
-from PyQt5.QtCore import QObject, pyqtSlot
 
 EXPO = 0.4
 
@@ -42,9 +41,6 @@ class Keyboard:
     
 class SteeringGUI:
   def __init__(self, steer_slider):
-      managed_processes['ui'].w.customWindow.emitSliderVal.connect(self.get_slider_value(val))
-      managed_processes['ui'].w.customWindow.accelDown.connect(self.GUI_ACCEL)
-      managed_processes['ui'].w.customWindow.accelRelease.connect(self.GUI_ACCEL)
       # Refers to the acceleration and steering inputs
       self.accel_axis = 'GUI_ACCEL'
       self.steer_axis = 'GUI_STEER'
