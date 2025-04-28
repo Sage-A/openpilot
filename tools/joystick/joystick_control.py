a9b7aa04941a709b002ce6912557caf367dadc79
aarondewaard20 <aarondewaard20@gmail.com>
2025-04-27 19:45:53 -0500

    Update joystick_control.py
    test z

--- tools/joystick/joystick_control.py
+++ tools/joystick/joystick_control.py
@@ -18,7 +18,7 @@ class Keyboard:
   def __init__(self):
     self.kb = KBHit()
     self.axis_increment = 0.05  # 5% of full actuation each key press
-    self.axes_map = {'w': 'gb', 's': 'gb',
+    self.axes_map = {'z': 'gb', 's': 'gb',
                      'a': 'steer', 'd': 'steer'}
     self.axes_values = {'gb': 0., 'steer': 0.}
     self.axes_order = ['gb', 'steer']
@@ -33,7 +33,7 @@ class Keyboard:
       self.cancel = True
     elif key in self.axes_map:
       axis = self.axes_map[key]
-      incr = self.axis_increment if key in ['w', 'a'] else -self.axis_increment
+      incr = self.axis_increment if key in ['z', 'a'] else -self.axis_increment
       self.axes_values[axis] = float(np.clip(self.axes_values[axis] + incr, -1, 1))
     else:
       return False
@@ -134,7 +134,7 @@ if __name__ == '__main__':
 
   print()
   if args.keyboard:
-    print('Gas/brake control: `W` and `S` keys')
+    print('Gas/brake control: `z` and `S` keys')
     print('Steering control: `A` and `D` keys')
     print('Buttons')
     print('- `R`: Resets axes')
